7609b551281367535724bc397a05aaf03bd7aae2
Taylor <tamynhier@mail.fhsu.edu>
2025-01-19 15:53:44 -0500

    Added rolling and tested framework

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 import random
 
 import discord
-from discord.ext import commands, tasks
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -25,12 +25,29 @@ async def on_ready():
     """ members = '\n - '.join([member.name for member in guild.members])
     print(f'Guild Members:\n - {members}') """
 
-# @bot.event
-# async def on_member_join(member):
-#     await  member.create_dm()
-#     await member.dm_channel.send(
-#         f'''Hi {member.name}, welcome to the Birth of a city discord server! Check out the 
-#         https://discord.com/channels/857435909343805490/1159623640434880613 channel for general server rules and the pinned 
-#         messages in https://discord.com/channels/857435909343805490/1160001533258256435. After you have done that we will get 
-#         you roles so you can access the rest of the server!'''
-#     )
+# dice roll command
+@bot.command()
+async def roll(ctx, *, text: str):
+    count, _, text = text.partition('d')  # eg "12d34+56" -> "12", "d", "34+56"
+    size, _, offset = text.partition('+')  # eg "34+56" -> "34", "+", "56"
+    if offset == "":
+        offset = 0
+    try:
+        count = int(count)
+        size = int(size)
+        offset = int(offset)
+    except ValueError:
+        raise commands.BadArgument('bad formatting of dice value')
+    return await _roll(ctx, count, size, offset)
+
+async def _roll(ctx, count, size, offset):
+    rolls = [random.randint(offset+1, offset+size) for i in range(count)]  # eg for 20 with offset 3, generate values 4 to 23
+    embed = discord.Embed(title=f'Dice for {count}d{size}+{offset}')
+    embed.description = '\n'.join((f'dice #{i+1}: **{roll}**' for i, roll in enumerate(rolls)))
+    embed.add_field(name='sum', value=f'total = {sum(rolls)}')
+    await ctx.send(embed=embed)
+    
+    
+    
+    
+bot.run(TOKEN)
